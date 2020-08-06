cebd9861b3577ad8a5fe5dfacd68fb69253dabfc
Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
2020-08-06 08:50:35 -0300

    PHP CS-fix

--- src/AbstractSingleton.php
+++ src/AbstractSingleton.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * This file is part of coisa/singleton.
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ *
+ * @link      https://github.com/coisa/singleton
+ * @copyright Copyright (c) 2020 Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+
 namespace CoiSA\Singleton;
 
 /**
